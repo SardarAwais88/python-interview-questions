6e4aa28a7e348b4d4ae07ac1e690b56ff40a55bf
SardarAwais88 <73728255+SardarAwais88@users.noreply.github.com>
2024-09-05 07:20:46 +0500

    Update

---
+++ class calling.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        print(f"{self.brand} {self.model} is created.")
+
+# Creating an object of Car class
+my_car = Car("Toyota", "Corolla")
+```
+
+### **Output:**
+```
+Toyota Corolla is created.
